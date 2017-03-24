fa9970afd4c0a167c67c62d5cbf50fc22d84aa61
Tom Rune Sæverås <tomrunesaeveras@hotmail.no>
2017-03-24 15:45:11 +0100

    okok

--- src/inf101/v17/boulderdash/bdobjects/BDSand.java
+++ src/inf101/v17/boulderdash/bdobjects/BDSand.java
@@ -22,7 +22,7 @@ public class BDSand extends AbstractBDObject {
 	
 	public BDSand(BDMap owner) {
 		super(owner);
-		InputStream resourceAsStream = getClass().getResourceAsStream("Sand.PNG");
+		InputStream resourceAsStream = getClass().getResourceAsStream("sand.png");
 		ImagePattern image = new ImagePattern(new Image(resourceAsStream), 0, 0, 1.0,1.0, true);
 		this.image=image;
 	}
